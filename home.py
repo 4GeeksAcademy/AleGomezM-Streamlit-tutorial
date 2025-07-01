eaee323307b857e8d934d0bab3dc1b75e21d90eb
Maria Alejandra Gomez <gogo_0192@hotmail.es>
2025-07-01 16:09:52 +0000

    avance

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Inicio")
+st.write("¡Bienvenido a la experiencia Sommelier!")
+
+st.write("Selecciona una opcion desde el menú lateral.")
+    
+
+
+# https://docs.streamlit.io/library/get-started/multipage-apps
+# Local: streamlit run streamlit_tutorial.py
+# Streamlit Sharing 
+# render, heroku, AWS EC2
---
+++ pages/Prediccion de vino.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from utils import predict_flores
+
+# Título de la aplicación
+st.title('Predicción de calidad de vino')
+st.image('calidad vino.jpg', caption='Imagen', use_column_width=True)
+
+
+
